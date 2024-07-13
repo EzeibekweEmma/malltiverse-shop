6b1a72b1880bc687935bc6396fbbe77cf6418b5e
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2024-07-13 12:21:50 +0100

    chore: Add ProductCard component for displaying product details

--- app/(tabs)/(product)/index.tsx
+++ app/(tabs)/(product)/index.tsx
@@ -13,6 +13,7 @@ import React, { useEffect, useState } from 'react';
 import Constants from 'expo-constants';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { FillStarIcon } from '@/assets/images/svgIcons';
+import ProductCard from './ProductCard';
 
 const ProductScreen = () => {
   const [data, setData] = useState([]);
@@ -147,41 +148,6 @@ const ProductScreen = () => {
         </SafeAreaView>
       </GestureHandlerRootView>
     </ScrollView>
-  );
-};
-
-const ProductCard = () => {
-  const heroImage = require('@/assets/images/heroImage.png');
-
-  return (
-    <View className="flex-col space-y-4 max-w-[160px]">
-      <Image source={heroImage} className="w-[160px] h-[160px] rounded-md" />
-
-      <View>
-        <Text className="font-[semi] text-lg">Tech Gadget</Text>
-        <Text
-          className="text-pryColor text-lg mt-0.5 font-[regular]"
-          numberOfLines={1}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec
-          odio vitae nunc.
-        </Text>
-
-        <View className="flex-row space-x-2 mt-1">
-          <FillStarIcon />
-          <FillStarIcon />
-          <FillStarIcon />
-          <FillStarIcon />
-          <FillStarIcon />
-        </View>
-
-        <Text className="text-secColor font-[semi] text-lg mt-2">N 20,000</Text>
-      </View>
-
-      <TouchableOpacity className="w-[90px] h-[35px] justify-center items-center rounded-xl border border-secColor">
-        <Text className="text-pryColor font-[semi]">Add to cart</Text>
-      </TouchableOpacity>
-    </View>
   );
 };
 
